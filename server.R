b70ddb99f1eb2af250161b88afb0ad23480e7cf9
liby-mathew <53029573+liby-mathew@users.noreply.github.com>
2019-07-27 17:01:43 +0530

    Update server.R

--- server.R
+++ server.R
@@ -18,9 +18,12 @@ shinyServer(function(input, output) {
   
   
   output$plot <- renderPlot({
-    if(is.null(input$txtFile) || is.null(input$modelFile)){
-      return(NULL)
+    if(is.null(input$txtFile)) {
+      datapath = "https://github.com/liby-mathew/text_summarization/blob/master/Chandrayaan2.txt"
     } else {
+#      if(is.null(input$modelFile)){
+ #       datapath = "https://github.com/liby-mathew/text_summarization/blob/master/english-ud-2.1-20180111.udpipe"
+  #    }
       textData = readLines(file(input$txtFile$datapath), encoding = "UTF-8")
       textData  =  str_replace_all(textData, "<.*?>", "") 
       model = udpipe_load_model(input$modelFile$datapath)  
@@ -50,47 +53,49 @@ shinyServer(function(input, output) {
       
     }
   })
+
+    
+  # output$wcloud <- renderPlot({
+  #   if(is.null(input$txtFile) || is.null(input$modelFile)){
+  #     return(NULL)
+  #   } else {
+  #     textData = readLines(file(input$txtFile$datapath), encoding = "UTF-8")
+  #     textData  =  str_replace_all(textData, "<.*?>", "") 
+  #     model = udpipe_load_model(input$modelFile$datapath)  
+  #     
+  #     x <- udpipe_annotate(model, x = textData) 
+  #     x <- as.data.frame(x)
+  #     
+  #     all_words = x %>% subset(., upos %in% c(input$checkbox)); all_words$token[1:20]
+  #     
+  #     all_words = txt_freq(all_words$lemma)
+  #     head(all_words$key, 20) 
+  #     
+  #     
+  #     wordcloud(words = all_words$key, 
+  #               freq = all_words$freq, 
+  #               min.freq = 2, 
+  #               max.words = 100,
+  #               random.order = FALSE,
+  #               scale=c(4,2),
+  #               colors = brewer.pal(8, "Dark2"),
+  #               main="Title")
+  #   }
+  # }, height = 800, width = 1200 )
+  # 
+  # output$datatable <- renderDataTable({
+  #   if(is.null(input$txtFile) || is.null(input$modelFile)){
+  #     return(NULL)
+  #   } else {
+  #     textData = readLines(file(input$txtFile$datapath), encoding = "UTF-8")
+  #     textData  =  str_replace_all(textData, "<.*?>", "") 
+  #     model = udpipe_load_model(input$modelFile$datapath)  
+  #     
+  #     x <- udpipe_annotate(model, x = textData) 
+  #     x <- as.data.frame(x)
+  #     out = data.frame(row_name = row.names(x),x)
+  #     out
+  #   }
+  # })
   
-  output$wcloud <- renderPlot({
-    if(is.null(input$txtFile) || is.null(input$modelFile)){
-      return(NULL)
-    } else {
-      textData = readLines(file(input$txtFile$datapath), encoding = "UTF-8")
-      textData  =  str_replace_all(textData, "<.*?>", "") 
-      model = udpipe_load_model(input$modelFile$datapath)  
-      
-      x <- udpipe_annotate(model, x = textData) 
-      x <- as.data.frame(x)
-      
-      all_words = x %>% subset(., upos %in% c(input$checkbox)); all_words$token[1:20]
-      
-      all_words = txt_freq(all_words$lemma)
-      head(all_words$key, 20) 
-      
-      
-      wordcloud(words = all_words$key, 
-                freq = all_words$freq, 
-                min.freq = 2, 
-                max.words = 100,
-                random.order = FALSE,
-                scale=c(4,2),
-                colors = brewer.pal(8, "Dark2"),
-                main="Title")
-    }
-  }, height = 800, width = 1200 )
-  
-  output$datatable <- renderDataTable({
-    if(is.null(input$txtFile) || is.null(input$modelFile)){
-      return(NULL)
-    } else {
-      textData = readLines(file(input$txtFile$datapath), encoding = "UTF-8")
-      textData  =  str_replace_all(textData, "<.*?>", "") 
-      model = udpipe_load_model(input$modelFile$datapath)  
-      
-      x <- udpipe_annotate(model, x = textData) 
-      x <- as.data.frame(x)
-      out = data.frame(row_name = row.names(x),x)
-      out
-    }
-  })
 })
